e547354bddcb6ecc88727fdc8fe37f366573dbb3
pennvi <pennwatsonsix@gmail.com>
2021-08-05 00:33:54 -0400

    bew fukes

---
+++ myjs.js
@@ -0,0 +1,25 @@
+let myReq = getXMLHttpRequest();
+
+function getXMLHttpRequest() {
+    return new XMLHttpRequest();
+}
+
+function callAjax() {
+    var url = "https://reqres.in/api/unknown";
+    var myRand = parseInt(Math.random() * 999999);
+    myReq.open("GET", url + "?rand=" + myRand, true);
+    myReq.onreadystatechange = responseAjax;
+    myReq.send(null);
+}
+
+
+function responseAjax() {
+    if(myReq.readyState == 4) {
+        alert(myReq.responseText)
+        var now = new Date;
+        var localTime = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        var serverTime = myReq.responseText;
+        document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br/>" + "Local: " + localTime;
+    }
+    else alert("gof uck yourself" + myReq.statusText)
+}
